b9e6aeb4d481a688e7b060f88cc1f8cda3517214
Charan Teja <charaneie22@gmail.com>
2018-05-10 03:35:35 +0530

    design Completed

---
+++ app/components/MapItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import MapView from 'react-native-maps';
+export default class MapItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+        
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
